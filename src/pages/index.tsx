f122543286df561b854517dfa0eefd43aac41c1a
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-01-12 17:35:57 +0530

    project inialised with Tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>KubeHack</title>
+        <meta name="title" content="KubeHack" />
+        <meta
+          name="description"
+          content="Kubehack is a hackathon with endless creativity. Students are challenged to further what they know by creating projects following their passions."
+        />
+        <meta name="keywords" content="Hackathons, KubeHack, kubesimplify" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="theme-color" content="#4B0082" />
+        <meta name="language" content="English" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="max-w-6xl mx-auto flex flex-col gap-4 mb-2">
+        hello
+      </main>
+    </>
+  );
+}
