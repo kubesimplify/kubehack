fd9dc4d1493c7a33222cf54bf5203b77da6f7d2a
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-01-12 20:38:01 +0530

    feat: Created Tracks Component

---
+++ src/components/Track.tsx
@@ -0,0 +1,40 @@
+import { tracks } from "@data/index";
+import Heading from "./ui/Heading";
+import SectionWrapper from "./ui/SectionWrapper";
+
+const Track = () => {
+  return (
+    <SectionWrapper id="track">
+      <Heading>Tracks</Heading>
+      <div className="flex flex-wrap gap-5 justify-center">
+        {tracks.map((track) => (
+          <TrackBlob key={track.id} {...track} />
+        ))}
+      </div>
+    </SectionWrapper>
+  );
+};
+
+const TrackBlob = ({
+  name,
+  backgroundImg,
+}: {
+  name: string;
+  backgroundImg: string;
+}) => (
+  <div className="relative group sm:w-48 sm:h-40 w-24 h-20 blob border-2 overflow-hidden">
+    <div
+      className="w-48 h-40 bg-center bg-contain overflow-hidden opacity-40"
+      style={{
+        backgroundImage: `url(${backgroundImg})`,
+      }}
+    ></div>
+    <p className=" absolute top-0 left-0 w-full h-full blob bg-light-background/5 group-hover:bg-cyan/20">
+      <span className="flex items-center justify-center sm:text-3xl h-full font-bold">
+        {name}
+      </span>
+    </p>
+  </div>
+);
+
+export default Track;
---
+++ src/data/index.ts
@@ -0,0 +1,305 @@
+import CivoLogo from '@assets/sponserLogo/civo.png'
+
+export const socials = {
+  twitter: "https://twitter.com/kubesimplify",
+  linkedin: "https://www.linkedin.com/company/kubesimplify/",
+  instagram: "",
+  discord: "https://discord.gg/26Z384WSPB",
+};
+
+export const tracks = [
+  {
+    id: 1,
+    name: "Blockchain",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1639762681485-074b7f938ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+  },
+  {
+    id: 2,
+    name: "IoT",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1558346490-a72e53ae2d4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 3,
+    name: "AR & VR",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1576633587382-13ddf37b1fc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+  {
+    id: 4,
+    name: "AI & ML",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1576633587382-13ddf37b1fc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+  {
+    id: 5,
+    name: "Cloud",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1577760258779-e787a1733016?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 6,
+    name: "Data Science",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
+  },
+  {
+    id: 7,
+    name: "Security",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1558346490-a72e53ae2d4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 8,
+    name: "Community",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 9,
+    name: "Health",
+    backgroundImg:
+      "https://images.unsplash.com/photo-1511174511562-5f7f18b874f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWljcm9zY29wZSUyMGRvY3RvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
+export const speakers = [
+  {
+    id: 1,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "t",
+      linkedin: "l",
+      instagram: "i",
+    },
+  },
+  {
+    id: 2,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 3,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 4,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 5,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 6,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 7,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 8,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 9,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 10,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 11,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 12,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 13,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 14,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+  {
+    id: 15,
+    name: "Speaker Name",
+    imageUrl: "https://avatars.githubusercontent.com/u/8190114?v=4",
+    socials: {
+      twitter: "",
+      linkedin: "",
+      instagram: "",
+    },
+  },
+];
+
+export const sponsers = [
+  {
+    id: 1,
+    sponserLogo: CivoLogo,
+  },
+  {
+    id: 2,
+    sponserLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+  },
+  {
+    id: 3,
+    sponserLogo: CivoLogo,
+  },
+  {
+    id: 4,
+    sponserLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+  },
+  {
+    id: 5,
+    sponserLogo: CivoLogo,
+  },
+  {
+    id: 6,
+    sponserLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+  },
+  {
+    id: 7,
+    sponserLogo: CivoLogo,
+  },
+  {
+    id: 8,
+    sponserLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+  },
+  {
+    id: 9,
+    sponserLogo: CivoLogo,
+  },
+  {
+    id: 10,
+    sponserLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+  },
+
+];
+
+export const faq = [
+  {
+    id: 1,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+  {
+    id: 2,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+  {
+    id: 3,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+  {
+    id: 4,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+  {
+    id: 5,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+  {
+    id: 6,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+  {
+    id: 7,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+  {
+    id: 8,
+    title: "How can we participate in the Hackathon?",
+    description: "Join our discord to know more."
+  },
+] 
