c1a31fc306b26ecf39c6d286969772004cedcd48
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-01-12 22:00:57 +0530

    Add: Created Hero Component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,6 +12,7 @@ const ButtonWithLink = ({
   return (
     <a
       className={`flex gap-2 items-center px-2 py-1 text-sm sm:px-4 sm:py-2 sm:text-lg border-2 border-white rounded-lg bg-light-background/10 hover:text-cyan hover:border-cyan cursor-pointer ${className}`}
+      target="_blank"
       {...props}
     >
       {children}
