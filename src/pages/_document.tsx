c25461637d0aeea55100ff520fd4e9efecb66a72
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-03-05 21:09:33 +0530

    fix: firefox scroll

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='relative bg-[#00072F] min-h-screen text-white overflow-hidden w-screen'>
+      <body className="relative bg-[#00072F] min-h-screen text-white overflow-x-hidden w-screen">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
