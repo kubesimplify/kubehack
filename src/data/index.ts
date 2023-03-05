3e18f7b2f8fc4b31dfad66c49efb6f6992641b3d
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-03-05 18:34:06 +0530

    changed tracks name to dummy

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,4 @@
-import CivoLogo from '@assets/sponserLogo/civo.png'
+import CivoLogo from "@assets/sponserLogo/civo.png";
 
 //! If anything you want to remove just set the value to null like this
 // date: 'March 25 - 26, 2023',
@@ -12,67 +12,68 @@ const hackathon = {
     discord: "https://discord.gg/26Z384WSPB",
   },
   email: "contact@kubehack.com",
-  location: 'Delhi NCR',
-  date: 'Coming Soon...',
-  aboutUs: 'KubeHack is the ultimate cloud native hackathon, bringing together developers, engineers and tech enthusiasts to create innovative solutions in the cloud native ecosystem. Our event offers mentorship, workshops, and prizes for those looking to learn, experiment and innovate in the cloud. Join us and be a part of the cloud native revolution!',
+  location: "Delhi NCR",
+  date: "Coming Soon...",
+  aboutUs:
+    "KubeHack is the ultimate cloud native hackathon, bringing together developers, engineers and tech enthusiasts to create innovative solutions in the cloud native ecosystem. Our event offers mentorship, workshops, and prizes for those looking to learn, experiment and innovate in the cloud. Join us and be a part of the cloud native revolution!",
   blur: {
     sponsors: true,
     speakers: true,
     prizes: true,
-    tracks: true
+    tracks: true,
   },
   tracks: [
     {
       id: 1,
-      name: "Automation",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1639762681485-074b7f938ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
     },
     {
       id: 2,
-      name: "Environment",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1558346490-a72e53ae2d4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 3,
-      name: "WASM",
+      name: "Coming Soon",
       backgroundImg:
         "https://miro.medium.com/max/828/1*3tuplcAChrIQQaSFRv-8fg.webp",
     },
     {
       id: 4,
-      name: "AI & ML",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1555255707-c07966088b7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFjaGluZSUyMGxlYXJuaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
     },
     {
       id: 5,
-      name: "Cloud",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1577760258779-e787a1733016?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 6,
-      name: "Open Innovation",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
     },
     {
       id: 7,
-      name: "Security",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1558346490-a72e53ae2d4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 8,
-      name: "Community",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 9,
-      name: "Health",
+      name: "Coming Soon",
       backgroundImg:
         "https://images.unsplash.com/photo-1511174511562-5f7f18b874f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWljcm9zY29wZSUyMGRvY3RvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     },
@@ -186,7 +187,8 @@ const hackathon = {
     },
     {
       id: 2,
-      sponsorLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+      sponsorLogo:
+        "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png",
     },
     {
       id: 3,
@@ -194,7 +196,8 @@ const hackathon = {
     },
     {
       id: 4,
-      sponsorLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+      sponsorLogo:
+        "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png",
     },
     {
       id: 5,
@@ -202,7 +205,8 @@ const hackathon = {
     },
     {
       id: 6,
-      sponsorLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+      sponsorLogo:
+        "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png",
     },
     {
       id: 7,
@@ -210,7 +214,8 @@ const hackathon = {
     },
     {
       id: 8,
-      sponsorLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
+      sponsorLogo:
+        "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png",
     },
     {
       id: 9,
@@ -218,46 +223,54 @@ const hackathon = {
     },
     {
       id: 10,
-      sponsorLogo: "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png"
-    }
+      sponsorLogo:
+        "https://cdn.discordapp.com/attachments/1011970131112505378/1062977570771042395/image.png",
+    },
   ],
   faq: [
     {
       id: 1,
       title: "What is a Hackathon?",
-      description: "A Hackathon is an event where individuals or teams of programmers and other tech enthusiasts come together to collaborate and develop innovative solutions to a specific problem or challenge."
+      description:
+        "A Hackathon is an event where individuals or teams of programmers and other tech enthusiasts come together to collaborate and develop innovative solutions to a specific problem or challenge.",
     },
     {
       id: 2,
       title: "Who can participate in a hackathon?",
-      description: "Anyone with a passion for technology and a desire to learn and create can participate in a hackathon. Hackathon is open to all skill levels and backgrounds."
+      description:
+        "Anyone with a passion for technology and a desire to learn and create can participate in a hackathon. Hackathon is open to all skill levels and backgrounds.",
     },
     {
       id: 3,
       title: "What should I bring to a hackathon?",
-      description: "You should bring your laptop or computer, chargers, any necessary cables, and any other tools or resources you may need to develop your project."
+      description:
+        "You should bring your laptop or computer, chargers, any necessary cables, and any other tools or resources you may need to develop your project.",
     },
     {
       id: 4,
       title: "What if I don't have a team?",
-      description: "Our hackathons have a team formation period at the beginning of the event where you can meet and collaborate with other participants. Alternatively, you can participate as an individual and join a team later on."
+      description:
+        "Our hackathons have a team formation period at the beginning of the event where you can meet and collaborate with other participants. Alternatively, you can participate as an individual and join a team later on.",
     },
     {
       id: 5,
       title: "What are the judging criteria?",
-      description: "The judging criteria vary depending on the hackathon, but typically include factors such as innovation, creativity, feasibility, and impact."
+      description:
+        "The judging criteria vary depending on the hackathon, but typically include factors such as innovation, creativity, feasibility, and impact.",
     },
     {
       id: 6,
       title: "How can I prepare for a hackathon?",
-      description: "You can prepare for a hackathon by researching the theme or challenge ahead of time, familiarizing yourself with relevant tools and technologies, and practicing your skills in advance. It can also be helpful to network with other participants and attend any pre-event workshops or meetups."
+      description:
+        "You can prepare for a hackathon by researching the theme or challenge ahead of time, familiarizing yourself with relevant tools and technologies, and practicing your skills in advance. It can also be helpful to network with other participants and attend any pre-event workshops or meetups.",
     },
     {
       id: 7,
       title: "What does it Cost?",
-      description: "KubeHack is free for all hackers! We provide our hackers with workshops, swags, and prizes without any cost through our generous sponsors."
+      description:
+        "KubeHack is free for all hackers! We provide our hackers with workshops, swags, and prizes without any cost through our generous sponsors.",
     },
   ],
-}
+};
 
-export default hackathon
+export default hackathon;
