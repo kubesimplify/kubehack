e9bb01e96c2b62bf883aeb03c4b219424f6e4e6a
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-03-07 12:34:45 +0530

    added coc page (#24)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,5 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
